714f4eff41f77126b230436122b16dc4a109d0f7
duyngo0605 <95336529+duyngo0605@users.noreply.github.com>
2023-10-21 14:34:38 +0700

    Set up connection to database

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     return res.send('Hello')
 })
+
+const mongoose=require("mongoose");
+
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_DB}@cluster0.63booln.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
+    console.log("connection success")
+});
+
+
+
+console.log(process.env.MONGO_DB);
 
 app.listen(port, () => {
     console.log('Server is running in port: ', + port)
