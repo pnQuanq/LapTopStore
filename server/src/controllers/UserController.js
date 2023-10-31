8d33471a2b88a360f6a2062db63f2e2f600d81f4
pnQuanq <phamquang13103@gmail.com>
2023-10-31 12:52:21 +0700

    API sign-in, sign-up

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -2,8 +2,9 @@ const UserService = require('../services/UserService')
 
 const createUser = async (req, res) => {
     try {
-        console.log(req.body)
-        const {name, email, password, confirmPassword, phone } = res.body
+        const { name, email, password, confirmPassword, phone } = req.body
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
         if(!name || !email|| !password|| !confirmPassword|| !phone) {
             return res.status(200).json({
                 status: 'ERR',
@@ -17,12 +18,11 @@ const createUser = async (req, res) => {
         }else if(password != confirmPassword) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'The input is equal'
+                message: 'The password is equal confirm password'
             })
         }
-        console.log('isCheckEmail', isCheckEmail)
-        const res = await UserService.createUser()
-        return res.status(200).json()
+        const response = await UserService.createUser(req.body)
+        return res.status(200).json(response)       
     }catch(e) {
         return res.status(404).json({
             message: e
@@ -30,6 +30,57 @@ const createUser = async (req, res) => {
     }
 }
 
+const loginUser = async (req, res) => {
+    try {
+        const { name, email, password, confirmPassword, phone } = req.body
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+        if(!name || !email|| !password|| !confirmPassword|| !phone) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        }else if(!isCheckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            })
+        }else if(password != confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirm password'
+            })
+        }
+        const response = await UserService.loginUser(req.body)
+        return res.status(200).json(response)       
+    }catch(e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id
+        const data = req.body
+        if(!userId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The user ID is required'
+            })
+        }
+        const response = await UserService.updateUser(userId, data)
+        return res.status(200).json(response)
+    } catch(e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    loginUser,
+    updateUser
 }
